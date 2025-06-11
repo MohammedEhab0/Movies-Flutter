15c7363438e8e646e8122fd3f468f3eb62c665a6
michaelabodahab <Michaelabodahab295@gmail.com>
2025-06-11 03:38:30 +0300

    add profile ui

---
+++ Movies-Flutter-Devolop/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:movies/ui/homescreen/home_screen.dart';
+
+import 'package:provider/provider.dart';
+
+import 'Providers/SettingProvider.dart';
+
+import 'utils/app_theme.dart';
+
+void main() async {
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en'),
+      startLocale: Locale('en'),
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => SettingProviders()),
+        ],
+        child: const MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var settingProviders = Provider.of<SettingProviders>(context);
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+      title: 'Movies',
+    );
+  }
+}
