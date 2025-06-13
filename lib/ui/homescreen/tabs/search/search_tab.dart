b10b7823cb88fdd0f447ad95b32c4f50701f1c56
MohammedEhab0 <moohammed092@gmail.com>
2025-06-14 01:06:34 +0300

    fix error

--- lib/ui/homescreen/tabs/search/search_tab.dart
+++ lib/ui/homescreen/tabs/search/search_tab.dart
@@ -1,12 +1,26 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:movies/UI/Widgets/CustomTextField.dart';
 
+import '../../../../utils/app_assets.dart';
 import '../../../../utils/app_styles.dart';
 
 class SearchTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(child: Text("Search",style: AppStyles.regular16yellow,)),
+      body: SafeArea(
+        child: Padding(
+          padding:EdgeInsets.symmetric(horizontal: width*.05),
+          child: Column(children: [   SizedBox(height: height * .02) ,CustomTextField(
+           // controller: viewModel.emailController,
+            prefixIcon: Image.asset(AppAssets.search),
+            hintText: 'search'.tr(),
+          ),],),
+        ),
+      ),
     );
   }
 }
